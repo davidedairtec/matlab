889430d3d1997a7e46cc4bad64e830ddfb47fd66
davidedairtec <dagualpa@edairtec.com>
2025-11-11 12:20:46 +0100

    nuevo archivo

---
+++ untitled2.m
@@ -0,0 +1,7 @@
+% Si Drive está en una unidad local (D:, E:, etc.)
+
+modelo_viejo = 'C:\Users\dagua\Music\h2aps\h2aps_version_final.slx';
+modelo_nuevo ='C:\Users\dagua\Videos\h2aps\h2aps_version_final.slx';
+
+
+visdiff(modelo_viejo, modelo_nuevo);
